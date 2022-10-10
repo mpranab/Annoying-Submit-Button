575c5c386a70fd893ed16379fda8efdca4ce4e50
mpranab <pranab-kumar.mohanta@capgemini.com>
2022-10-10 13:17:38 +0530

    Floating Submit Button using ReactJS.

---
+++ Annoying_submit/annoying-submit/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer style={{ textAlign: 'center' }}>
+      All &copy; Rights Reserved!
+      <p style={{ marginTop: '2px' }}>
+        Follow Pranab Mohanta on{' '}
+        <a href="https://www.linkedin.com/in/pranab-kumar-mohanta-094a51128/">LinkedIn</a> and{' '}
+        <a href="https://github.com/mpranab">GitHub</a> for more.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
